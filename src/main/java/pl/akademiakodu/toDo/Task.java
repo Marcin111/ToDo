a275e40d8abac66a380603b9289b9f31c786a41d
Marcin111 <marcin4412@gmail.com>
2018-08-11 12:35:13 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/toDo/Task.java
@@ -0,0 +1,42 @@
+package pl.akademiakodu.toDo;
+
+public class Task {
+    private String name;
+    private String description;
+    private boolean isFinished;
+
+    public Task(String name, String description, boolean isFinished) {
+        this.name = name;
+        this.description = description;
+        this.isFinished = isFinished;
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+}
