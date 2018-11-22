4771456ea11fb46a0a917226ad9b1855246edd67
Marcin111 <marcin4412@gmail.com>
2018-11-22 21:05:01 +0100

    no //

--- src/main/java/pl/akademiakodu/toDo/Task.java
+++ src/main/java/pl/akademiakodu/toDo/Task.java
@@ -5,15 +5,6 @@ public class Task {
     private String description;
     private boolean isFinished;
 
-//    @Override
-//    public String toString() {
-//        return "Task{" +
-//                "name='" + name + '\'' +
-//                ", description='" + description + '\'' +
-//                ", isFinished=" + isFinished +
-//                '}';
-//    }
-
     public Task(String name, String description, boolean isFinished) {
         this.name = name;
         this.description = description;
